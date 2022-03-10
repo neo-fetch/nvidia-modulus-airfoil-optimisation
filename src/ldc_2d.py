7680fc7b9a2ac4637bc90d98be9663226a40d2bc
neo-fetch <mayank18529@mechydd.ac.in>
2022-03-10 04:59:59 +0000

    Successfully running with inputs from Dr. Yang

--- src/ldc_2d.py
+++ src/ldc_2d.py
@@ -1,5 +1,4 @@
-from sympy import Symbol, Eq, Ge, Abs, Function, Number
-import sympy as sp
+from sympy import Symbol, Eq, Ge, Abs, Function, Number, sin, cos
 from modulus.pdes import PDES
 from modulus.variables import Variables
 import time
@@ -30,10 +29,10 @@ class NavierStokes_2D(PDES):
         x, y = Symbol('x'), Symbol('y')
 
         # angle of attack
-        alp = Symbol('alpha')
-        
+        alpha = Symbol('alpha')
+
         # make input variables
-        input_variables = {'x': x, 'y': y, 'alpha': alp}
+        input_variables = {'x': x, 'y': y, 'alpha': alpha}
 
         # velocity componets
         u = Function('u')(*input_variables)
@@ -47,15 +46,12 @@ class NavierStokes_2D(PDES):
         # Here I implement a simpler form of a 2D Navier-Stokes equation in the form of laplacian(u,v) = 0 such that
         # laplacian(u,v).diff(u) = u and laplacian(u,v).diff(v) = v
         # laplacian(u,v).diff(t) = 0
-        self.equations['x_alpha'] = u - 10*sp.cos(alp)
-        self.equations['y_alpha'] = v - 10*sp.sin(alp)
         self.equations['x_component'] = u-phi.diff(x)
         self.equations['y_component'] = v-phi.diff(y)
         self.equations['NavierStokes_2D'] = (phi.diff(x)).diff(x) + (phi.diff(y)).diff(y) # grad^2(phi)
 
 
 # params for domain
-magnitude = 10
 obstacle_length = 0.10
 height = 6*obstacle_length  
 # Honestly, we can set the height to anything as long 
@@ -76,15 +72,16 @@ wake.rotate(np.pi / 2)
 geo = rec
 
 # define sympy varaibles to parametize domain curves
-x, y = Symbol("x"), Symbol("y")
-alp = Symbol("alpha")
+x, y, alpha = Symbol('x'), Symbol('y'), Symbol('alpha')
 # limit the range of alpha from -10 to 10 using np.pi.
-alpha_range = {alp, (-np.pi*10/180, np.pi*10/180)}
-# u, v = get_angle(alp, 10)
+param_ranges = {alpha, (-np.pi*10/180, np.pi*10/180)}
+fixed_param_range = {alpha: lambda batch_size: np.full((batch_size, 1), np.random.uniform(-np.pi*10/180, np.pi*10/180))}
+
+# u, v =  get_angle(np.pi*10/180, 10)
 # u = float(sys.argv[1])
 # v = float(sys.argv[2])
-print(f"u = {alp}, v = {alp}")
-time.sleep(1)
+# print(f"u = {u}, v = {v}")
+# time.sleep(1)
 
 class LDCTrain(TrainDomain):
     def __init__(self, **config):
@@ -103,38 +100,45 @@ class LDCTrain(TrainDomain):
         #  / ////////////
         #    / //////////
         # where / is u + v such that tan-1(v/u) = x degrees(here i kept x as 4).
-
+        u_x = 10*cos(alpha)
+        u_y = 10*sin(alpha)
+        flow_rate = u_x*width + u_y*height
         inletBC = geo.boundary_bc(
-            outvar_sympy={"u": sp.cos(alp)*magnitude, "v": sp.sin(alp)*magnitude},
+            outvar_sympy={"u": u_x, "v": u_y},
             batch_size_per_area=1000,
             criteria=Eq(x, -width / 2),
-            param_ranges = alpha_range
+            param_ranges ={**fixed_param_range},
+            fixed_var=False
         )
         self.add(inletBC, name="Inlet")
 
         # outlet
         outletBC = geo.boundary_bc(
-            outvar_sympy={"integral_continuity": u*height+v*width, "u": sp.cos(alp)*magnitude, "v": sp.sin(alp)*magnitude}, # Mimicing the far field conditions
+            outvar_sympy={"integral_continuity": flow_rate, "u":u_x , "v": u_y}, # Mimicing the far field conditions
             batch_size_per_area=256,
             criteria=Ge(y/height+x/width, 1/2),
-            param_ranges = alpha_range
+            param_ranges ={**fixed_param_range},
+            fixed_var=False
         )
         self.add(outletBC, name="Outlet")
 
         # bottomWall
         bottomWall = geo.boundary_bc(
-            outvar_sympy={"u": sp.cos(alp)*magnitude, "v": sp.sin(alp)*magnitude},
+            outvar_sympy={"u": u_x, "v": u_y},
             batch_size_per_area=1000,
             criteria=Eq(y, -height / 2),
-            param_ranges = alpha_range
+            param_ranges ={**fixed_param_range},
+            fixed_var=False            
         )
         self.add(bottomWall, name="BottomWall")
 
         # obstacleLine
         obstacleLine = obstacle.boundary_bc(
-            outvar_sympy={"u": sp.cos(alp)*magnitude, "v": 0},
+            outvar_sympy={"u": u_x, "v": 0},
             batch_size_per_area=1000,
             lambda_sympy={"lambda_u": 100, "lambda_v": 100},
+            param_ranges ={**fixed_param_range},
+            fixed_var=False            
         )
         self.add(obstacleLine, name="obstacleLine")
 
@@ -142,10 +146,11 @@ class LDCTrain(TrainDomain):
         # Here we define u = u and v = 0 at the trailing edge of the obstacle(which is at x=0, and v = v at x = right wall).
         l = lambda x : (x)/(3*obstacle_length) # x = 0 at the trailing edge of the obstacle
         wakeLine = wake.boundary_bc(
-            outvar_sympy={"u": sp.cos(alp)*magnitude, "v": sp.sin(alp)*magnitude*l(x)},
+            outvar_sympy={"u": u_x, "v": u_y*l(x)},
             batch_size_per_area=1000,
             lambda_sympy={"lambda_u": 100, "lambda_v": 100},
-            param_ranges = alpha_range
+            param_ranges ={**fixed_param_range},
+            fixed_var=False            
         )
         self.add(wakeLine, name="wakeLine")
 
@@ -159,7 +164,8 @@ class LDCTrain(TrainDomain):
                 "lambda_y_component": geo.sdf,
             },
             batch_size_per_area=10000,
-            param_ranges = alpha_range
+            param_ranges ={**fixed_param_range},
+            fixed_var=False            
         )
         self.add(interior, name="Interior")
 
@@ -172,69 +178,20 @@ class LDCTrain(TrainDomain):
                 "lambda_y_component": geo.sdf,
             },
             batch_size_per_area=10000,
-            param_ranges = alpha_range
+            param_ranges ={**fixed_param_range},
+            fixed_var=False            
         )
         self.add(neighbourhood, name="Neighbourhood")
 
 
-        # planes
-       # plane1Cont = plane1.boundary_bc(
-       #     outvar_sympy={"integral_continuity": 0.1333333},
-       #     batch_size_per_area=256,
-       #     lambda_sympy={"lambda_integral_continuity": 10},
-       # )
-       # plane2Cont = plane2.boundary_bc(
-       #     outvar_sympy={"integral_continuity": 0.1333333},
-       #     batch_size_per_area=256,
-       #     lambda_sympy={"lambda_integral_continuity": 10},
-       # )
-       # plane3Cont = plane3.boundary_bc(
-       #     outvar_sympy={"integral_continuity": 0.1333333},
-       #     batch_size_per_area=256,
-       #     lambda_sympy={"lambda_integral_continuity": 10},
-       # )
-       # plane4Cont = plane4.boundary_bc(
-       #     outvar_sympy={"integral_continuity": 0.1333333},
-       #     batch_size_per_area=256,
-       #     lambda_sympy={"lambda_integral_continuity": 10},
-       # )
-       # plane5Cont = plane5.boundary_bc(
-       #     outvar_sympy={"integral_continuity": 0.1333333},
-       #     batch_size_per_area=256,
-       #     lambda_sympy={"lambda_integral_continuity": 10},
-       # )
-       # plane6Cont = plane6.boundary_bc(
-       #     outvar_sympy={"integral_continuity": 0.1333333},
-       #     batch_size_per_area=256,
-       #     lambda_sympy={"lambda_integral_continuity": 10},
-       # )
-       # plane7Cont = plane7.boundary_bc(
-       #     outvar_sympy={"integral_continuity": 0.1333333},
-       #     batch_size_per_area=256,
-       #     lambda_sympy={"lambda_integral_continuity": 10},
-       # )
-
-       # self.add(plane1Cont, name="integralContinuity1")
-       # self.add(plane2Cont, name="integralContinuity2")
-       # self.add(plane3Cont, name="integralContinuity3")
-       # self.add(plane4Cont, name="integralContinuity4")
-       # self.add(plane5Cont, name="integralContinuity5")
-       # self.add(plane6Cont, name="integralContinuity6")
-       # self.add(plane7Cont, name="integralContinuity7")
-
-
 # validation data
-mapping = {"Points:0": "x", "Points:1": "y", "U:0": "u", "U:1": "v", "p": "p"}
-openfoam_var = csv_to_dict("openfoam/cavity_uniformVel0.csv", mapping)
-openfoam_var["x"] += -width / 2  # center OpenFoam data
-openfoam_var["y"] += -height / 2  # center OpenFoam data
-openfoam_invar_numpy = {
-    key: value for key, value in openfoam_var.items() if key in ["x", "y"]
-}
-openfoam_outvar_numpy = {
-    key: value for key, value in openfoam_var.items() if key in ["u", "v"]
-}
-
+# validation data
+mapping = {'Points:0': 'x', 'Points:1': 'y', 'Points:2': 'alpha','U:0': 'u', 'U:1': 'v', 'p': 'p'}
+openfoam_var = csv_to_dict('openfoam/cavity_uniformVel0.csv', mapping)
+openfoam_var['x'] += -width / 2  # center OpenFoam data
+openfoam_var['y'] += -height / 2  # center OpenFoam data
+openfoam_invar_numpy = {key: value for key, value in openfoam_var.items() if key in ['x', 'y', 'alpha']}
+openfoam_outvar_numpy = {key: value for key, value in openfoam_var.items() if key in ['u', 'v']}
 
 class LDCVal(ValidationDomain):
     def __init__(self, **config):

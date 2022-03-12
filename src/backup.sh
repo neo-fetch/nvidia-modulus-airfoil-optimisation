9bdadc0a1cfa4846d0a405948520f581b4759c21
neo-fetch <mayank18529@mechyd.ac.in>
2022-03-12 14:11:57 +0530

    made a more stable script

--- src/backup.sh
+++ src/backup.sh
@@ -8,8 +8,9 @@
 # done
 while true
 do
-    steps=$(cat /examples/ldc/network_checkpoint_ldc_2d/checkpoint | grep model_checkpoint_path: | grep -Eo '[0-9]*')
-    # echo "steps: ${steps}"
+    steps=$(cat ~/simnet/examples/ldc/network_checkpoint_ldc_2d/checkpoint | grep model_checkpoint_path: | grep -Eo '[0-9]*')
+    echo "steps: ${steps}"
+    sleep $((10))
     # convert steps to integer
     steps=$(($steps))
     # check if steps is divisible by 20000
@@ -17,6 +18,7 @@ do
         echo "20000 steps done. Backing up files..."
         # backup files
         echo "Copying checkpoint at ${steps}..."
-        cp -r "~/simnet/examples/ldc/network_checkpoint_ldc_2d/train_domain/" "~/simnet/results/train_domain_${steps}"
+        cp -r ~/simnet/examples/ldc/network_checkpoint_ldc_2d/train_domain ~/simnet/results/train_domain_${steps}
+        sleep $((2*60))
     fi
 done

532f45983c92bf5392b427dac2f414da9515f0b1
neo-fetch <mayank18529@mechyd.ac.in>
2022-04-24 19:55:41 +0000

    Added sid's subroutine

--- src/kd_tree.py
+++ src/kd_tree.py
@@ -1,16 +1,13 @@
 ### AUTHOR ###
 # Name: Siddharth Agarwal
 # Description:
-
-
-
-
+# ---> This is the temp version of K-D Tree algorithm. Will be used in integration with the original LDC code
 from tokenize import Double
 import numpy as np
 from sklearn.neighbors import KDTree
 import time
 
-def kd_Tree(X, D, N, n, point, U):
+def kd_Tree(X, D, N, n, point, Du, U):
     # ------------------------------ PART - A (Finding the n-nearest neighbours) ------------------------------
 
     #Reading N points from csv file and adding it in X (Numpy 2D Array).
@@ -71,14 +68,52 @@ def kd_Tree(X, D, N, n, point, U):
 
 
 
+    # ------------------------------ PART - B (Finding the n-nearest neighbours) ------------------------------
+
+    #Reading N points from csv file and adding it in X (Numpy 2D Array).
+    # N_points = open("pointcloud_coordinates.csv")
+    # X = np.loadtxt(N_points, delimiter=",")
+
+    # D = int(input("Enter the geometric dimensionility of the problem (D): "))
+    if(D!=len(X[0])):
+        print("ERROR: The Input pointcloud_coordinates.csv file contain points of dimension (D) = ", len(X[0]))
+        print("Quit!!")
+        quit()
+
+    N = int(input("Enter the total number of points (N): "))
+    if(N!=len(X)):
+        print("ERROR: Total number of points in input pointcloud_coordinates.csv file (N) = ", len(X))
+        print("Quit!!")
+        quit()
+    print("Reading the data from pointcloud_coordinates.csv file ...")
+
+    # This is used if user has to enter all N points manually on terminal. Ignore, since input is given through csv file.
+    # print("Enter all the point coordinates:")
+    # X_mat = [list(map(float,input().split())) for i in range(N)]
+    # X = np.array(X_mat)
+
+    n = int(input("Enter the number of neighbour points (n): "))
+
+    point = list(map(float, input("Enter the point of interest (x): ").split()))
+    if(D!=len(point)): #Verifying whether the input point x has dimension D or not.
+        print("ERROR: Point must be of dimension = ", D)
+        print("Quit!!")
+        quit()
+
+    start = time.time()
+
+    #The 2 lines below uses the python inbuilt library for KD-Tree.
+    tree = KDTree(X)
+    dist, ind = tree.query([point], k=n) 
+    #dist
 
     # ------------------------------ PART - B (Interpolation) ------------------------------
 
     #Reading all u points from csv file and adding it in U (Numpy 2D Array).
-    U_points = open("pointcloud_u_vector.csv")
-    U = np.loadtxt(U_points, delimiter=",")
+    # U_points = open("pointcloud_u_vector.csv")
+    # U = np.loadtxt(U_points, delimiter=",")
 
-    Du = int(input("Enter the dimensionality of u(phi) function (Du): "))
+    # Du = int(input("Enter the dimensionality of u(phi) function (Du): "))
     print("Reading the data from pointcloud_u_vector.csv file ...")
 
     #This array is used to store all the interpolated values, if Du >= 2.
@@ -122,7 +157,7 @@ def kd_Tree(X, D, N, n, point, U):
             if(flag_val==0 and ux_denom!=0):
                 interpolated_ux_arr[j] = ux_numer/ux_denom
 
-            print("Interpolated u(x) component",j+1," = ",interpolated_ux_arr[j])
+            print("Interpolated u(x) component", j+1, " = ", interpolated_ux_arr[j])
 
     print()
 
